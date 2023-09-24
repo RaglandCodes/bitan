367906b1ba72e08f2929c20efae550a4da3de9d5
Ragland Asir <raglandasir@gmail.com>
2023-09-24 14:23:08 +0530

    Set up to use space

--- website/static/website/scripts/globalScripts.js
+++
@@ -1,1 +0,0 @@
-console.log('global scripts are here');
--- website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('auth/signin/', views.sign_in, name='signin'),
-    path('paraPartial/', views.paraPartial, name='paraPartial')
-]
